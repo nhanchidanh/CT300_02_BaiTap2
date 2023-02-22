d42e40aab882d422e1d46dffe6fd5f77ea2c5df8
nhanchidanh <nhanchidanh@gmail.com>
2023-02-22 15:40:31 +0700

    modified hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+        System.out.println("Hello World!\n"); 
     }
 }
